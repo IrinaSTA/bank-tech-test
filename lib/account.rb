12d931ea6a5d521643e4f8fe405f11566ea98a82
Irina Baldwin <2007inb@gmail.com>
2018-12-03 14:42:31 +0000

    Add deposit and withdraw methods

--- lib/account.rb
+++ lib/account.rb
@@ -4,6 +4,31 @@ class Account
 
   def initialize
     @balance = 0
+    @transactions = []
+  end
+
+  def deposit(amount, date)
+    record_transaction(amount, date, :debit)
+    increase_balance(amount)
+  end
+
+  def withdraw(amount, date)
+    record_transaction(amount, date, :credit)
+    decrease_balance(amount)
+  end
+
+  private
+
+  def record_transaction(amount, date, type)
+    @transactions.push({amount: amount, date: date, type: type})
+  end
+
+  def increase_balance(amount)
+    @balance += amount
+  end
+
+  def decrease_balance(amount)
+    @balance -= amount
   end
 
 end
